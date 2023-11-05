a116d384e44f64c6a7a611ce86732167a6cf1fd8
wangxiaoze@petalmail.com <wangxiaoze@petalmail.com>
2023-11-05 16:27:38 +0800

    feat: 添加时间

--- src/shared/send.ts
+++ src/shared/send.ts
@@ -1,10 +1,13 @@
 import logReporting from '../index';
 import { getNavigator } from './navigator';
+import dayjs from 'dayjs';
 
 function getCommonError() {
   return {
     title: document.title,
     url: location.href,
+    date: dayjs(Date.now()).format('YYYY-MM-DD'),
+    dateTime: dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
     ...getNavigator(),
   };
 }
