d627b7f5b06f20daa3f1db2812297c8f1c75bd5e
王小泽 <wangze@anbangke.com>
2024-01-03 15:01:40 +0800

    re: 大数据进行压缩

--- src/lib/watchRecord.ts
+++ src/lib/watchRecord.ts
@@ -8,7 +8,30 @@ export function watchRecord() {
             isCheckout && (logReporting.snapshot = [])
             logReporting.snapshot.push(event);
         },
-        checkoutEveryNms: 0.25 * 60 * 1000, // 每15s重新制作快照
-        // checkoutEveryNth: 200, // 每 200 个 event 重新制作快照
+        checkoutEveryNms: 15 * 1000, // 每15s重新制作快照
+        checkoutEveryNth: 200, // 每 200 个 event 重新制作快照
+        // packFn: rrweb.pack,
+        sampling: {
+            // 不录制鼠标移动事件
+            mousemove: false,
+            // 设置滚动事件的触发频率
+            scroll: 150, // 每 150ms 最多触发一次
+            // set the interval of media interaction event
+            media: 800,
+            // 设置输入事件的录制时机
+            input: 'last', // 连续输入时，只录制最终值
+            // 定义不录制的鼠标交互事件类型，可以细粒度的开启或关闭对应交互录制
+            mouseInteraction: {
+                MouseUp: false,
+                MouseDown: false,
+                Click: false,
+                ContextMenu: false,
+                DblClick: false,
+                Focus: false,
+                Blur: false,
+                TouchStart: false,
+                TouchEnd: false,
+            },
+        }
     });
 }
--- src/shared/send.ts
+++ src/shared/send.ts
@@ -1,6 +1,7 @@
 import logReporting from '../index';
 import { getNavigator } from './navigator';
 import dayjs from 'dayjs';
+import lz from 'lz-string'
 
 function getCommonError() {
   return {
@@ -8,7 +9,9 @@ function getCommonError() {
     url: location.href,
     date: dayjs(Date.now()).format('YYYY-MM-DD'),
     dateTime: dayjs(Date.now()).format('YYYY-MM-DD HH:mm:ss'),
-    snapshot: JSON.stringify(logReporting.snapshot),
+    // snapshot: JSON.stringify(logReporting.snapshot),
+    // 这里使用 lz-string进行压缩, 但是在返回的数据哪里需要解压
+    snapshot: lz.compress(JSON.stringify(logReporting.snapshot)),
     ...getNavigator(),
   };
 }
