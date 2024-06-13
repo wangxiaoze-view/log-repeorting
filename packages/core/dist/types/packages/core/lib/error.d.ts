13ed41cb62db8ad51c34d45fde13bc34d4b95e54
王小泽 <wangze@anbangke.com>
2024-06-13 11:00:25 +0800

    docs(package): 更新导入文件

--- packages/core/dist/types/packages/core/lib/error.d.ts
+++ packages/core/dist/types/packages/core/lib/error.d.ts
@@ -8,22 +8,16 @@ export declare function getErrorStackLine(e: Error): Partial<IConsoleErrorType>;
 export declare function getErrorElInfo(elTarget?: Node | Element): IELBindType;
 export declare function getError(e: ErrorEvent): IErrorType;
 export declare function getPromiseError(e: PromiseRejectionEvent): IErrorType;
-export declare function getXhrError(
-  e: Event,
-  questOptions: IXhrOpenType,
-): IErrorType & {
-  questOptions: IXhrOpenType;
+export declare function getXhrError(e: Event, questOptions: IXhrOpenType): IErrorType & {
+    questOptions: IXhrOpenType;
 };
-export declare function getFetchError(
-  questOptions: IXhrOpenType,
-  otherOptions?: Partial<IConsoleErrorType>,
-): {
-  errorMessage?: string | undefined;
-  time?: number | undefined;
-  colno?: number | undefined;
-  lineno?: number | undefined;
-  stackMessage?: string | undefined;
-  fileName?: string | undefined;
-  questOptions: IXhrOpenType;
-  el: IELBindType;
+export declare function getFetchError(questOptions: IXhrOpenType, otherOptions?: Partial<IConsoleErrorType>): {
+    errorMessage?: string | undefined;
+    time?: number | undefined;
+    colno?: number | undefined;
+    lineno?: number | undefined;
+    stackMessage?: string | undefined;
+    fileName?: string | undefined;
+    questOptions: IXhrOpenType;
+    el: IELBindType;
 };
